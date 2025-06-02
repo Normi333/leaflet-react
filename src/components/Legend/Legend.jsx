ff0d0bb34184e5b14265ff89fd12d24b9f320638
Normi333 <shisirthapa146@gmail.com>
2025-06-02 22:26:50 +0545

    Legend Rendering

--- src/components/Legend/Legend.jsx
+++ src/components/Legend/Legend.jsx
@@ -1,35 +1,3 @@
-// import "./Legend.css";
-
-// function Legend() {
-//   return (
-//     <div className="legend-box">
-//       <strong>Legend</strong>
-
-//       <div className="legend-item">
-//         <span className="legend-color district" />
-//         District Boundary
-//       </div>
-
-//       <div className="legend-item">
-//         <span className="legend-color roads" />
-//         Roads
-//       </div>
-
-//       <div className="legend-item">
-//         <span className="legend-color sagarmatha" />
-//         Sagarmatha National Park
-//       </div>
-
-//       <div className="legend-item">
-//         <span className="legend-color makalu" />
-//         Makalu Barun National Park
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Legend;
-
 import "./Legend.css";
 
 const legendItems = {
