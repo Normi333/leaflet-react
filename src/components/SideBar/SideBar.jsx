0e0b1a1bade7a8e04a0ac07e5fe88e275bbb5796
Normi333 <shisirthapa146@gmail.com>
2025-06-02 16:49:11 +0545

    Sidebar revamped

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,45 +1,78 @@
-// components/SideBar.jsx
-import React from "react";
+import React, { useState } from "react";
 import "./SideBar.css";
 
 function SideBar() {
+  const [openSection, setOpenSection] = useState(null);
+
+  const toggleSection = (section) => {
+    setOpenSection(openSection === section ? null : section);
+  };
+
   return (
     <div className="sidebar">
-      <h3>
-        <img src="/map_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg" alt="Map" />
-        Base
-      </h3>
-      <label className="checkbox-text">
-        <input type="checkbox" name="base" value="base1" /> District Boundary
-      </label>
+      <ul className="sidebar-menu">
+        {/* Base Section */}
+        <li className="menu-title" onClick={() => toggleSection("base")}>
+          <img
+            src="/map_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"
+            alt="Base"
+          />
+          Base
+        </li>
+        {openSection === "base" && (
+          <ul className="submenu">
+            <li>
+              <label className="checkbox-text">
+                <input type="checkbox" name="base" value="district" /> District
+                Boundary
+              </label>
+            </li>
+          </ul>
+        )}
 
-      <h3>
-        <img
-          src="/timeline_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"
-          alt="Map"
-        />
-        Infrastructure
-      </h3>
-      <label className="checkbox-text">
-        <input type="checkbox" name="buildingLayer" value="infra1" />
-        Roads
-      </label>
-      <h3>
-        <img
-          src="/explore_nearby_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"
-          alt="Map"
-        />
-        POI - Point Of Interest
-      </h3>
-      <label className="checkbox-text">
-        <input type="checkbox" name="transportLayer" value="poi1" /> Sagarmatha
-        National Park
-      </label>
-      <br />
-      <label className="checkbox-text">
-        <input type="checkbox" name="transportLayer" value="poi2" /> Makalu
-        Barun National Park
-      </label>
+        {/* Infrastructure Section */}
+        <li className="menu-title" onClick={() => toggleSection("infra")}>
+          <img
+            src="/timeline_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"
+            alt="Infra"
+          />
+          Infrastructure
+        </li>
+        {openSection === "infra" && (
+          <ul className="submenu">
+            <li>
+              <label className="checkbox-text">
+                <input type="checkbox" name="infra" value="roads" /> Roads
+              </label>
+            </li>
+          </ul>
+        )}
+
+        {/* POI Section */}
+        <li className="menu-title" onClick={() => toggleSection("poi")}>
+          <img
+            src="/explore_nearby_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"
+            alt="POI"
+          />
+          POI - Point Of Interest
+        </li>
+        {openSection === "poi" && (
+          <ul className="submenu">
+            <li>
+              <label className="checkbox-text">
+                <input type="checkbox" name="poi" value="sagarmatha" />{" "}
+                Sagarmatha National Park
+              </label>
+            </li>
+            <li>
+              <label className="checkbox-text">
+                <input type="checkbox" name="poi" value="makalu" /> Makalu Barun
+                National Park
+              </label>
+            </li>
+          </ul>
+        )}
+      </ul>
     </div>
   );
 }
